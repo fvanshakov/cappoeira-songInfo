261b21926313ee3bc77b9225d2344fb67160c76c
Fedor Anshakov <fvanshakov@yandex.ru>
2025-03-24 16:51:03 +0300

    added full-text search through lucine

--- songInfo/songInfoDB/build.gradle.kts
+++ songInfo/songInfoDB/build.gradle.kts
@@ -2,6 +2,8 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.postgresql:postgresql")
+    implementation("org.hibernate.search:hibernate-search-mapper-orm:7.2.0.Final")
+    implementation("org.hibernate.search:hibernate-search-backend-lucene:7.2.0.Final")
 
     testImplementation(kotlin("test"))
     testImplementation("org.springframework.boot:spring-boot-starter-test")
--- songInfo/songInfoDB/src/main/kotlin/ru/cappoeira/songInfo/songInfoDB/entity/SongInfoEntity.kt
+++ songInfo/songInfoDB/src/main/kotlin/ru/cappoeira/songInfo/songInfoDB/entity/SongInfoEntity.kt
@@ -1,17 +1,25 @@
 package ru.cappoeira.songInfo.songInfoDB.entity
 
 import jakarta.persistence.*
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed
 import ru.cappoeira.songInfo.emptyString
 
 
 @Entity
+@Indexed
 @Table(name = "songs")
 data class SongInfoEntity(
     @Id
     var id: String,
 
-    @Column(nullable = false)
+    @FullTextField
+    @Column(nullable = false, name = NAME)
     var name: String
 ) {
     constructor(): this(emptyString(), emptyString())
+
+    companion object {
+        const val NAME = "name"
+    }
 }
