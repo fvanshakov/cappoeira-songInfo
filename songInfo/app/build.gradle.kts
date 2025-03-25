0902acd684fe03391392ff6c5bbeae19f34dfba1
fvanshakov <fvanshakov@avito.ru>
2025-03-26 01:23:53 +0300

    added swagger

--- songInfo/app/build.gradle.kts
+++ songInfo/app/build.gradle.kts
@@ -1,7 +1,8 @@
 dependencies {
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
     implementation("org.springframework.boot:spring-boot-starter")
-    implementation(project(":adminBoardClient"))
 
+    implementation(project(":adminBoardClient"))
     implementation(project(":songInfoDB"))
     implementation(project(":utils"))
 }
