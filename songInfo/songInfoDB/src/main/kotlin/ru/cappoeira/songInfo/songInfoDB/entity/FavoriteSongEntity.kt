08399926890fc382c546917e3c8820e135913bca
fvanshakov <fvanshakov@avito.ru>
2025-12-21 21:23:49 +0300

    fixed fav sons

--- songInfo/songInfoDB/src/main/kotlin/ru/cappoeira/songInfo/songInfoDB/entity/FavoriteSongEntity.kt
+++ songInfo/songInfoDB/src/main/kotlin/ru/cappoeira/songInfo/songInfoDB/entity/FavoriteSongEntity.kt
@@ -1,21 +1,27 @@
 package ru.cappoeira.songInfo.songInfoDB.entity
 
 import jakarta.persistence.*
-import ru.cappoeira.songInfo.emptyString
+import java.io.Serializable
+
+@Embeddable
+data class FavoriteSongId(
+    var userId: String = "",
+    var songId: String = ""
+) : Serializable
 
 @Entity
 @Table(name = "favorite_songs")
 data class FavoriteSongEntity(
-    @Id
-    @Column(nullable = false)
-    var userId: String,
+    @EmbeddedId
+    var id: FavoriteSongId = FavoriteSongId(),
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("songId")
     @JoinColumn(name = "song_id", nullable = false)
-    var song: SongInfoEntity,
+    var song: SongInfoEntity = SongInfoEntity(),
 
     @Column(nullable = false)
     var addedAt: Long = System.currentTimeMillis()
 ) {
-    constructor(): this(emptyString(), SongInfoEntity())
+    constructor(): this(FavoriteSongId(), SongInfoEntity())
 }
