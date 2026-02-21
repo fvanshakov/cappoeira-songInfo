41858e9879107426dabf1788d62e9a051a08abf7
fvanshakov <fvanshakov@avito.ru>
2026-02-21 12:45:00 +0300

    migrating to nocodb

--- songInfo/adminBoardClient/src/main/kotlin/ru/cappoeira/songInfo/adminBoardClient/dtos/AdminBoardDefinitionsDto.kt
+++ songInfo/adminBoardClient/src/main/kotlin/ru/cappoeira/songInfo/adminBoardClient/dtos/AdminBoardDefinitionsDto.kt
@@ -3,7 +3,7 @@ package ru.cappoeira.songInfo.adminBoardClient.dtos
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class AdminBoardDefinitionsDto(
-    @JsonProperty("Id")
+    @JsonProperty("id_1")
     val clientId: String?,
     @JsonProperty("definition")
     val definition: String?
